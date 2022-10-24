a3e1611cb8ba606fd3c2b3fa1e2e057d492a9a2e
Kiridepapel <monalek0622@hotmail.com>
2022-10-24 18:59:54 -0500

    sprint initializr

---
+++ src/main/java/pe/edu/upc/translogic/TranslogicApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.translogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/translogic/TranslogicApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.translogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
